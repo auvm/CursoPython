873a658236b46f859bcf547b83507814797fb0f4
auvm <angelurielvelascom@gmail.com>
2025-01-25 01:03:18 -0600

    add 06 to 07 folders, constants and numbers basics

---
+++ 06 - Constantes/main.py
@@ -0,0 +1,9 @@
+"""
+No existe un tipo de dato CONSTANTE, 
+pero se usa la nomenclatura TODO_EN_MAYUSCULAS,
+para dar a entender que no deberíamos 
+modificarla y solo usarla para lectura.
+"""
+
+VERSION = 3.14
+print(VERSION)
