85a369bd9f2e803c3a2e50ccc81ce38f46fe4fc7
auvm <angelurielvelascom@gmail.com>
2025-02-21 00:09:16 -0600

    add 11 and 12 folders, variables & data types

---
+++ 12 - Multiples variables/main.py
@@ -0,0 +1,17 @@
+#De forma tradicional se declaran variables así:
+#first_name = "uriel"
+#age = 13
+#is_active = True
+
+"""
+Pero para reducir a una linea la declaración y asignación 
+de valores a variables, se pueden hacer en horizontal,
+se asignan los valores en el orden respectivo en el que 
+se declararon.
+"""
+first_name, age, is_active = "auvm", 20, True
+
+print(first_name)
+print(age)
+print(is_active)
+
