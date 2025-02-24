6ea15a55ebe76ae9e224d4a3cc44cae8566556be
auvm <angelurielvelascom@gmail.com>
2025-02-23 22:10:12 -0600

    add 01 and 02, workig with lists

--- 02 - Comentarios/main.py
+++
@@ -1,9 +0,0 @@
-#Ésta línea es un comentario
-print("Hola, mundo!");
-
-""" Otra forma de comentar, 
-	es mediante triples comillas dobles.
-		Esto permite ocupar múltiples saltos
-			de líneas"""
-
-
--- 06 - Constantes/main.py
+++
@@ -1,9 +0,0 @@
-"""
-No existe un tipo de dato CONSTANTE, 
-pero se usa la nomenclatura TODO_EN_MAYUSCULAS,
-para dar a entender que no deberíamos 
-modificarla y solo usarla para lectura.
-"""
-
-VERSION = 3.14
-print(VERSION)
