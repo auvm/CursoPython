d88aae4d0415ede616de9a088210edcf06e7860b
auvm <angelurielvelascom@gmail.com>
2026-02-15 18:05:25 -0600

    primer commit

---
+++ 02-variebles-y-tipos/variebles.py
@@ -0,0 +1,17 @@
+"""
+una variebl es un contenedor de información
+que dentro guardará un dato, se pueden crear 
+varias variables y que cada uno tenga un dato distinto.
+"""
+
+texto = "Master en python"
+texto2 = texto.upper()
+numero = 45
+decimal = 3.14
+booleano = True
+
+print(texto)
+print(texto2)
+print(numero)
+print(decimal)
+print(booleano)
